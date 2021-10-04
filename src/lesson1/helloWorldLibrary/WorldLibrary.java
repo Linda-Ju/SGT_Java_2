a04179b16921d4403b6da093a5fa63aa5bd96346
Linda-Ju <linda.junkina@gmail.com>
2021-10-04 21:07:06 +0300

    Initial commit

---
+++ src/lesson1/helloWorldLibrary/WorldLibrary.java
@@ -0,0 +1,17 @@
+package lesson1.helloWorldLibrary;
+
+public class WorldLibrary {
+    private String planetName;
+
+    public static String getName(){
+        return "Linda";
+    }
+
+    public String getPlanetName() {
+        return planetName;
+    }
+
+    public void setPlanetName(String planetName) {
+        this.planetName = planetName;
+    }
+}
