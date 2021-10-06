04a18efbfc3aa823838ace087d041a84ba46439f
Linda-Ju <linda.junkina@gmail.com>
2021-10-06 21:31:05 +0300

    Lesson 2

---
+++ src/lesson2/syntax/Fundamentals.java
@@ -0,0 +1,19 @@
+package lesson2.syntax;
+
+public class Fundamentals {
+    // single line comment
+    /* multiple lines
+    comment
+     */
+
+    /**
+     * someMethod should be used in following cases - when the method should be used
+     * @param param1 - name of the user
+     * @param param2
+     * @return
+     */
+    public String someMethod(String param1, int param2){
+        return "something";
+    }
+
+}
