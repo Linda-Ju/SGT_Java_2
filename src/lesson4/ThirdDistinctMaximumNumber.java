523abd2b0191a43aad221ce8ade70d9b4b9a19e5
Linda-Ju <linda.junkina@gmail.com>
2021-10-09 15:31:22 +0300

    Lesson 4 Practical Tasks FizzBuzz + ThirdMaxNumber

--- src/lesson4/ThirdDistinctMaximumNumber.java
+++ src/lesson4/ThirdDistinctMaximumNumber.java
@@ -42,7 +42,36 @@ public class ThirdDistinctMaximumNumber {
 //        } else {
 //            System.out.println(arr[arr.length - 1]);
 //        }
+
+            // 3d solution
+//            getLargestNumber(nums, nums.length);
+//            getLargestNumber(numsTwo, numsTwo.length);
+//            getLargestNumber(numsThree, numsThree.length);
         }
+
+//        public static void getLargestNumber(int[] array, int arraySize) {
+//            if (arraySize < 3 && arraySize >= 1) {
+//                int max = array[0];
+//                int i;
+//
+//                for (i = 1; i < array.length; i++) {
+//                    if (array[i] > max)
+//                        max = array[i];
+//                }
+//                System.out.printf("The Largest element is %d\n", max);
+//            } else if (arraySize >= 3) {
+//                int max;
+//                for (int i = 0; i < arraySize; i++) {
+//                    for (int j = i + 1; j < arraySize; j++) {
+//                        if (array[i] > array[j]) {
+//                            max = array[i];
+//                            array[i] = array[j];
+//                            array[j] = max;
+//                        }
+//                    }
+//                }
+//                System.out.printf("The third largest element is %d\n", array[arraySize - 3]);
+//            }
     }
 }
 
